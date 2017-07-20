3261e52aa8a71b3928c9969987b6b1989334237a
yfmartinez09 <dumsoft.servicios@gmail.com>
2017-07-20 12:29:13 -0400

    cndiguracion del correo

---
+++ advanced/common/config/main-local.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=db_yii2_advance',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+			'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'cemglobal2016@gmail.com',
+                'password' => 'PS77411_1',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
+        ],
+    ],
+];
