15fd85c94f05e85934ab94a49da977e9b55cca9e
yfmartinez09 <dumsoft.servicios@gmail.com>
2017-07-21 12:18:00 -0400

    config email for contact form

--- advanced/common/config/main-local.php
+++ advanced/common/config/main-local.php
@@ -7,23 +7,6 @@ return [
             'username' => 'root',
             'password' => '',
             'charset' => 'utf8',
-        ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-			// temp
-            'useFileTransport' => false,
-			'transport' => [
-                'class' => 'Swift_SmtpTransport',
-                'host' => 'smtp.gmail.com',
-                'username' => 'cemglobal2016@gmail.com',
-                'password' => 'PS77411_1',
-                'port' => '587',
-                'encryption' => 'tls',
-            ],
-        ],
-    ],
+        ],        
+    ]
 ];
--- advanced/common/config/main.php
+++ advanced/common/config/main.php
@@ -1,9 +1,27 @@
 <?php
+
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            // temp
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'cemglobal2016@gmail.com',
+                'password' => 'PS77411_1',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
         ],
     ],
 ];
