59eb59d2f6b2f07b07fb67648fb84bd80b07db64
yfmartinez09 <dumsoft.servicios@gmail.com>
2017-07-21 17:25:10 -0400

    COnfig general

--- advanced/common/config/params.php
+++ advanced/common/config/params.php
@@ -1,6 +1,6 @@
 <?php
 return [
-    'adminEmail' => 'admin@example.com',
+    'adminEmail' => 'yfmartinez@gmail.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
 ];
